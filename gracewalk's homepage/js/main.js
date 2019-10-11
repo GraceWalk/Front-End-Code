5fa8eb8ba563d533f94b803217c5156c931ad932
GraceWalk <gracewalk1999@163.com>
2019-10-11 21:22:17 +0800

    web作业

--- gracewalk's homepage/js/main.js
+++ gracewalk's homepage/js/main.js
@@ -1,8 +1,23 @@
-//谷歌搜索
+window.onLoad = function() {//谷歌搜索
 let search = document.querySelector('#search');
+search.onfocus = function() {
+    search.style.opacity = 0.6;
+}
+search.onblur = function() {
+    search.style.opacity = 0.3;
+}
+search.focus();
 search.onkeypress = function(e) {
     if (e.key === 'Enter') {
         window.open('http://www.google.com/search?q='+search.value)
-        searchBox.value = ''
+        search.value = ''
     }
+}
+
+let myButton = document.querySelector('#mybotton');
+myButton.addEventListener('click', function() {
+    let request = new XMLHttpRequest();
+    request.open('GET', '/xxx');
+    request.send();
+});
 }
